8771fb4bb5df5df5983b0953c05a33a0a33769c4
chn001118 <chn001118@gmail.com>
2023-08-16 14:26:41 +0800

    登录

--- lib/controller/index.dart
+++ lib/controller/index.dart
@@ -80,6 +80,7 @@ class Controller extends GetxController {
     print(islogin);
     if (islogin) {
       print('已登录');
+      print('当前钱包地址:${DB.box.read('token')}');
     } else {
       dapp.signMessage();
     }
